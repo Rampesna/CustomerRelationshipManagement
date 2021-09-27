29278c7814108b06a256013aa94b60b6607ecd00
Talha Can <rampesna@gmail.com>
2021-09-27 09:37:54 +0300

    v2.9.16.4

--- routes/ajax.php
+++ routes/ajax.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::post('setSelectedCompany', [\App\Http\Controllers\Ajax\DashboardController::class, 'setSelectedCompany'])->name('ajax.setSelectedCompany');
 
 Route::prefix('dashboard')->group(function () {
